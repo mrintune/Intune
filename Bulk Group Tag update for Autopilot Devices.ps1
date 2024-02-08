b1bbf227d9fabc060fbfc092aa6a1eec018a00b6
Mr Intune <LUVMYLYF12@GMAIL.COM>
2024-02-08 12:26:05 +0530

    Add files via upload

---
+++ Bulk Group Tag update for Autopilot Devices.ps1
@@ -0,0 +1,34 @@
+﻿Connect-MSGraph
+Update-MSGraphEnvironment -SchemaVersion "Beta" -Quiet
+Connect-MSGraph -Quiet
+
+#Change the content path with location of txt file in Line6
+
+$Serialnumbers = Get-Content 'C:\Users\Testing\Desktop\Bulk group tag change\Targeted device serial numbers.txt'
+$autopilotDevices = Invoke-MSGraphRequest -HttpMethod GET -Url "deviceManagement/windowsAutopilotDeviceIdentities" | Get-MSGraphAllPages
+
+#Change the Group Tag in Line18 
+
+foreach($autopilotDevice in $autopilotDevices)
+{
+foreach($Serialnumber in $serialnumbers)
+{
+if($autopilotDevice.serialNumber -eq $Serialnumber)
+{
+Write-Host "Matched, adding group tag to serial number" + $Serialnumber
+$autopilotDevice.groupTag = "US"
+$requestBody=
+@"
+    {
+        groupTag: `"$($autopilotDevice.groupTag)`",
+    }
+"@
+
+Invoke-MSGraphRequest -HttpMethod POST -Content $requestBody -Url "deviceManagement/windowsAutopilotDeviceIdentities/$($autopilotDevice.id)/UpdateDeviceProperties" 
+}
+else
+{
+write-host "Skipping Serial Number " + $Serialnumber
+}
+}
+}
