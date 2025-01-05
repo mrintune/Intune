ad5cc88fb3ea375ddc2be804b0e71b7be99290bd
MR INTUNE <LUVMYLYF12@GMAIL.COM>
2025-01-05 17:27:47 +0100

    Update Bulk Group Tag update for Autopilot Devices.ps1

--- Bulk Group Tag update for Autopilot Devices.ps1
+++ Bulk Group Tag update for Autopilot Devices.ps1
@@ -1,34 +1,44 @@
-Install-Module -Name MgGraph -force
-Import-Module -Name MgGraph -Force
-Update-MSGraphEnvironment -SchemaVersion "Beta" -Quiet
-Connect-MSGraph
+# Bulk Update Windows Autopilot Group Tag using PowerShell with Graph API
+# MR INTUNE Jan 2025
 
-#Change the content text filepath
-
-$Serialnumbers = Get-Content 'C:\Users\Testing\Desktop\Bulk group tag change\Targeted device serial numbers.txt'
-$autopilotDevices = Invoke-MSGraphRequest -HttpMethod GET -Url "deviceManagement/windowsAutopilotDeviceIdentities" | Get-MSGraphAllPages
-
-#Change the GroupTag as per your environment GroupTag
-foreach($autopilotDevice in $autopilotDevices)
-{
-foreach($Serialnumber in $serialnumbers)
-{
-if($autopilotDevice.serialNumber -eq $Serialnumber)
-{
-Write-Host "Matched, adding group tag to serial number" + $Serialnumber
-$autopilotDevice.groupTag = "US"
-$requestBody=
-@"
-    {
-        groupTag: `"$($autopilotDevice.groupTag)`",
-    }
-"@
-
-Invoke-MSGraphRequest -HttpMethod POST -Content $requestBody -Url "deviceManagement/windowsAutopilotDeviceIdentities/$($autopilotDevice.id)/UpdateDeviceProperties" 
+# Check if PackageProvider is installed
+if (-not (Get-PackageProvider -Name Nuget -ListAvailable -ErrorAction Ignore)) {
+    # Install Microsoft.Graph module
+    Install-PackageProvider -Name Nuget -confirm:$false -force
 }
-else
-{
-write-host "Skipping Serial Number " + $Serialnumber
+
+# Check if Microsoft.Graph module is installed
+if ( -not (Get-Module -Name Microsoft.Graph -ListAvailable -ErrorAction Ignore)) {
+    # Install Microsoft.Graph module
+    Install-Module -Name Microsoft.Graph -confirm:$false -force
 }
+
+# Check if WindowsAutoPilotIntune module is installed
+if (-not (Get-Module -Name WindowsAutoPilotIntune -ListAvailable -ErrorAction Ignore)) {
+    # Install WindowsAutoPilotIntune module
+    Install-Module -Name WindowsAutoPilotIntune -confirm:$false -force
+}
+# Permissions Assigned to the User for MgGraph Auth "Group.ReadWrite.All, Device.ReadWrite.All, DeviceManagementManagedDevices.ReadWrite.All, DeviceManagementServiceConfig.ReadWrite.All, GroupMember.ReadWrite.All"
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+Import-Module -Name Microsoft.Graph -force
+Import-Module -Name WindowsAutoPilotIntune -force
+Connect-MgGraph 
+# get list of serial numbers from CSV file
+$FilePathLocation = Read-Host "Enter Serials text file path location"
+$serialNumbers = Get-Content -Path $FilePathLocation
+# for each serial number, get entra device object id
+foreach ($serialNumber in $serialNumbers){
+try
+{
+$id = (Get-AutopilotDevice -serial $serialNumber).id
+#Change the NewGroupTag value as per your environment 
+$NewGrouptag = "US"
+Set-AutopilotDevice -id $id -GroupTag $NewGrouptag
+Write-Host "Added GroupTag: $($NewGrouptag) to serialNumber: $($serialNumber) "
+}
+catch
+{
+$message = $_.Exception.Message
+Write-Host "Failed to add GroupTag to SerialNumber $($serialNumber): $message"
 }
 }
